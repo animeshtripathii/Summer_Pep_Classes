035bdc7b27aab555299494fc20281f662201f79d
AnimeshLPU <tripathianimesh38@gmail.com>
2026-07-02 15:18:13 +0530

    implemented promise

--- Promise/index.js
+++ Promise/index.js
@@ -54,3 +54,65 @@ Promise.any([promise]).then((result) => {
 }).catch(() => {
     console.log("No fulfilled promises");
 });
+
+
+
+//promsie code for callback 
+
+
+function login(username, password) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (username && password) {
+                console.log("✔ Logged in successfully.");
+                resolve({ userId: "user_101", token: "xyz123" });
+            } else {
+                reject("Login Failed: Missing credentials");
+            }
+        }, 1000);
+    });
+}
+function getUserDetails(userId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`✔ Fetched details for User ID: ${userId}`);
+            resolve({ userId: userId, name: "Animesh", email: "animesh@example.com" });
+        }, 1000);
+    });
+}
+
+function createOrder(userId, item) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`✔ Order placed for ${item}.`);
+            resolve({ orderId: "ord_999", userId: userId, item: item });
+        }, 1000);
+    });
+}
+
+function getOrderDetails(orderId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`✔ Fetched receipt for Order ID: ${orderId}`);
+            resolve({ orderId: orderId, status: "Delivered", total: "₹1,499" });
+        }, 1000);
+    });
+}
+login("animesh_tripathi", "securePassword123")
+    .then((authData) => {
+        return getUserDetails(authData.userId);
+    })
+    .then((userData) => {
+        return createOrder(userData.userId, "Mechanical Keyboard");
+    })
+    .then((orderData) => {
+        return getOrderDetails(orderData.orderId);
+    })
+    .then((finalReceipt) => {
+        console.log("--- Final Order Receipt ---", finalReceipt);
+    })
+    .catch((error) => {
+        console.error("An error occurred in the chain:", error);
+    });
+
+
